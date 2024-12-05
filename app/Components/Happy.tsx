5f7d30839bab6b926683cca93565c77f7090d7f2
sujalcharati <sujalc024@gmail.com>
2024-12-05 09:21:07 +0530

    happy route

---
+++ app/Components/Happy.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export const Happy: React.FC = () => {
+    return (
+        <div>
+            it's happy page
+        </div>
+    );
+}
---
+++ app/happy/page.tsx
@@ -0,0 +1,7 @@
+
+import { Happy as Happycomponent } from "../Components/Happy"
+export default function happy(){
+    return <div>
+        <Happycomponent/>
+    </div>
+}
